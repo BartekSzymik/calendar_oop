0d4d7d1706ab0568dce6d714fa92c11ce49e6737
Bartosz Szymik <szymik.bartek@gmail.com>
2022-05-14 14:03:07 +0200

    add jason  data for calendar

--- data/utils.py
+++ data/utils.py
@@ -1,6 +1,6 @@
-from datetime import datetime, timedelta,
+from datetime import datetime, timedelta
 from random import random, randint
-
+import json
 
 class EventGenerator:
     def __init__(self):
@@ -14,9 +14,37 @@ class EventGenerator:
             event = Event(
                 title=random.choice(self.titles),
                 location=random.choice(self.locations),
-                start_time=f{'datetime.now() + timedelta(minutes=random.randint(50,50000))')},
+                start_time=f'{('datetime.now() + timedelta(minutes=random.randint(50,50000)))}'',
                 duration=random.randint(16,599),
                 owner=random.choice(self.users),
                 participants=random.choices(self.users, k=random.randint(0, len(self.users)))
             )
+
             self.events.append(event)
+
+    def generate_data(self, quantity=100):
+        events_data = []
+
+        for _ in range(quantity):
+            event = {
+                "title": random.choice(self.titles),
+                "location": random.choice(self.locations),
+                "start_time": f'{(datetime.now() + timedelta(minutes=random.randint(50, 500000))):%d-%m-%Y %H:%M}',
+                "duration": random.randint(16, 599),
+                "owner": random.choice(self.users),
+                "participants": random.choices(self.users, k=random.randint(0,
+                                                                            len(self.users)))
+            }
+            events_data.append(event)
+
+        return events_data
+
+    def save(self, path):
+        with open(path, "w") as file:
+            data = json.dumps(self.events, indent=4)
+            file.write(data)
+
+    @staticmethod
+    def load(path):
+        with open(path, 'r') as file:
+            return json.load(file)
--- main.py
+++ main.py
@@ -5,13 +5,17 @@ from data.utils import EventGenerator
 from event import Event
 
 data = EventGenerator()
-data.generate(500)
+#data.save('./data.json')
+events_data = data.load('./data.json')
 
-c = Calendar
-
-result = c.get_events_by_date("17-05-2022 8:00")
-
-# print(result)
-
-c.remove_event('17-05-2022 9:00')
-print(c)
+events = [Event(**event) for event in events_data]
+c = Calendar(events)
+result = c.get_events_by_date("01-01-2023 00:00")
+print(result, len(result))
+#
+# result = c.get_events_by_date("17-05-2022 8:00")
+#
+# # print(result)
+#
+# c.remove_event('17-05-2022 9:00')
+# print(c)
